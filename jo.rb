47dbc5b2ff8bb872ff9414a77a43ea8f32ac5fee
JP Mens <jpmens@gmail.com>
2016-03-10 18:36:32 +0100

    version 0.9

--- jo.rb
+++ jo.rb
@@ -2,8 +2,8 @@ class Jo < Formula
   desc "JSON output from a shell"
   homepage "https://github.com/jpmens/jo"
 
-  url "https://github.com/jpmens/jo/releases/download/v0.7/jo-0.7.tar.gz"
-  sha256 "3c948e5035ecd90666e4cae6ed9eaead709b5a482f2d9f788a81be5a06e14176"
+  url "https://github.com/jpmens/jo/releases/download/v0.9/jo-0.9.tar.gz"
+  sha256 "e757d17469e5c9682ebdd8c7a88b7231303bd0fca2a69ffedbcf24835eaa241f"
 
   head do
     url "https://github.com/jpmens/jo.git"
