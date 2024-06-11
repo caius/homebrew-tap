65851d56d8746cefce57e5dfa6bdd43e0c3ed18d
Caius Durling <dev@caius.name>
2024-06-11 11:28:31 +0100

    wip

--- Formula/dropserver.rb
+++ Formula/dropserver.rb
@@ -2,19 +2,33 @@ class Dropserver < Formula
   version "0.13.2"
   sha256 "2976b3fed485afe7faaef2700fe47c6914121083524d9889bfbddfeb4a6f1463"
 
-  url "https://github.com/teleclimber/Dropserver/releases/download/v#{version}/ds-dev-amd64-darwin.tar.gz"
+  # url "https://github.com/teleclimber/Dropserver/archive/v#{version}.tar.gz"
   desc "An application platform for your personal web services."
   homepage "https://dropserver.org/"
   license "apache-2.0"
 
-  #head "https://github.com/teleclimber/Dropserver.git", branch: "master"
+  head "https://github.com/teleclimber/Dropserver.git", branch: "master"
 
   livecheck do
     url :url
   end
 
+  depends_on "go" => :build
+  depends_on "node" => :build
+  depends_on "yarn" => :build
+
   def install
-    bin.install "ds-dev"
+    Dir.chdir("frontend-ds-dev") do
+      system "yarn install"
+      system "yarn run build"
+    end
+    system "scripts/build-ds-dev.sh"
+
+    Dir.chdir("frontend-ds-host") do
+      system "yarn install"
+      system "yarn run build"
+    end
+    system "scripts/build-ds-host.sh"
   end
 
   test do
