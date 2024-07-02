c7e217a700fa967f97c8c0fba564a833f7ff1b59
Caius Durling <dev@caius.name>
2024-07-02 10:48:29 +0100

    Add Immersed mac app as cask

---
+++ Casks/immersed.rb
@@ -0,0 +1,19 @@
+cask "immersed" do
+  version "21.1.2"
+  sha256 "12531166528d352cade049dc3c7de62259169c25aa3033408cf1b760a7b6b547"
+
+  url "https://static.immersed.com/dl/Immersed.dmg"
+  name "immersed"
+  desc "Empower work with Spatial Computing"
+  homepage "https://immersed.com/"
+
+  app "Immersed.app"
+
+  uninstall quit: "team.Immersed", kext: "team.Immersed.VirtualCamera"
+
+  zap trash: [
+    "~/Library/Application Support/Immersed",
+    "~/Library/Preferences/team.Immersed.plist",
+    "~/Library/Caches/team.Immersed"
+  ]
+end
