40bf5f1c4dfb87edc11bfa9c434a7067f0ed8b54
Caius Durling <dev@caius.name>
2025-12-24 18:02:15 +0000

    Put version number in `url`, bump-formula-pr updates it now

--- Formula/amp-code.rb
+++ Formula/amp-code.rb
@@ -2,7 +2,7 @@ class AmpCode < Formula
   version "0.0.1766563889-gdc4216"
   sha256 "30949f4ee4f13e42f9377cd5f33f8e789bfc9f8e19b05d6cdc660ae255dcfcba"
 
-  url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/#{version}/amp-darwin-arm64"
+  url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1766563889-gdc4216/amp-darwin-arm64"
   desc "Frontier coding agent that lets you wield the full power of leading models"
   homepage "https://ampcode.com/"
 
