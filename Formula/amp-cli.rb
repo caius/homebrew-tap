0fb78aa0445932b789f987d2723b643c019e502a
Caius Durling <caius@users.noreply.github.com>
2025-12-25 08:18:21 +0000

    amp-cli 0.0.1766649687-ge52779

--- Formula/amp-cli.rb
+++ Formula/amp-cli.rb
@@ -1,9 +1,9 @@
 class AmpCli < Formula
   desc "Frontier coding agent that lets you wield the full power of leading models"
   homepage "https://ampcode.com/"
-  url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1766594901-gbadae7/amp-darwin-arm64"
-  version "0.0.1766594901-gbadae7"
-  sha256 "d09de236dbd9eb0225476ee4c9d0cc417a68315c03c411a6b0251b1ff2170af4"
+  url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1766649687-ge52779/amp-darwin-arm64"
+  version "0.0.1766649687-ge52779"
+  sha256 "00c5b1cfa00c2fe4ba9b6d1e6de01efe7ab0ea7756888aa189b98460cab86879"
 
   livecheck do
     url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/cli-version.txt"
