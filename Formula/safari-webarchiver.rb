dd0a86ca429b33a46b01cd3582dfe3284f078f0e
Caius Durling <dev@caius.name>
2025-12-31 14:20:28 +0000

    Fix linting issues in SafariWebarchiver formula

--- Formula/safari-webarchiver.rb
+++ Formula/safari-webarchiver.rb
@@ -1,8 +1,8 @@
 class SafariWebarchiver < Formula
   desc "Create Safari webarchive files on the command-line"
   homepage "https://github.com/alexwlchan/safari-webarchiver"
+  url "https://github.com/alexwlchan/safari-webarchiver/archive/813f371f917e630bf6b300b58875f1e267616640.tar.gz"
   version "1.0.1"
-  url "https://github.com/alexwlchan/safari-webarchiver/archive/813f371f917e630bf6b300b58875f1e267616640.zip"
   sha256 "2ab97343a1db18077efc2b55a9c946cfb398048550e3d30fc5f346e415cd5c6f"
   license "MIT"
 
