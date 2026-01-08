8716ef9abd3ea112305f668989361fd21cddb3e5
Anka <runner@sat12-dp146-10a4ebeb-02de-4252-a45e-91d568f38c3f-2E45373B3AEB.local>
2026-01-08 08:18:36 +0000

    si 20260108.014547.0-sha.2ed72fdc

--- Formula/si.rb
+++ Formula/si.rb
@@ -1,9 +1,9 @@
 class Si < Formula
   desc "System Initiative CLI tool for managing schemas, templates, and components"
   homepage "https://systeminit.com/"
-  url "https://github.com/systeminit/si/archive/refs/tags/release/20260106.225402.0-sha.cb218ff6.tar.gz"
-  version "20260106.225402.0-sha.cb218ff6"
-  sha256 "6a92d6c0477c0d14f27e2f0ffc7db1f4822614fd136e31d39a5cc2c8cc0c5629"
+  url "https://github.com/systeminit/si/archive/refs/tags/release/20260108.014547.0-sha.2ed72fdc.tar.gz"
+  version "20260108.014547.0-sha.2ed72fdc"
+  sha256 "a34fdeffc05b213113c564c4a6cfa44269e037fe0c22895f0840d733c8b245a0"
   license "Apache-2.0"
 
   livecheck do
