dcb8ea84ac8afd57266a1c9cfaff6b8c53b76fc5
Caius Durling <dev@caius.name>
2026-01-24 15:43:49 +0000

    Add cask for Lolgato v1.7.0

---
+++ Casks/lolgato.rb
@@ -0,0 +1,23 @@
+cask "lolgato" do
+  version "1.7.0"
+  sha256 "ca64bdfdf7e62409e56d3fc08afd65e6cea40d6eb90751f2c69d5fcb62115395"
+
+  url "https://github.com/raine/Lolgato/releases/download/v#{version}/Lolgato.dmg"
+  name "Lolgato"
+  desc "Enhanced control over Elgato Lights"
+  homepage "https://github.com/raine/Lolgato"
+
+  livecheck do
+    url :url
+    strategy :github_releases
+  end
+
+  app "Lolgato.app"
+
+  uninstall quit: "fi.zendit.Lolgato"
+
+  zap trash: [
+    "~/Library/Preferences/fi.zendit.Lolgato.plist",
+    "~/Library/Caches/fi.zendit.Lolgato"
+  ]
+end
