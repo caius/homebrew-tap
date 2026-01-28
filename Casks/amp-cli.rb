e8ae16bf4f5a10c3ed2cfc53bcb09ddd9fcfb665
Anka <runner@sjc22-bt143-c4f4705e-e87a-456a-b88d-520b582eee98-EAF2B81391A5.local>
2026-01-28 08:18:32 +0000

    amp-cli 0.0.1769587291-gc70924

--- Casks/amp-cli.rb
+++ Casks/amp-cli.rb
@@ -1,6 +1,6 @@
 cask "amp-cli" do
-  version "0.0.1769500896-g45eb39"
-  sha256 "76231fc0420e71e39be6d5e41cccb6581fe53753953ffd1a435da067f8cdb8c6"
+  version "0.0.1769587291-gc70924"
+  sha256 "26ef5efbc8e8248c9427cd387498f3eb0dc767617c2090a6b81362ec0802af6f"
 
   url "https://storage.googleapis.com/amp-public-assets-prod-0/cli/#{version}/amp-darwin-arm64",
       verified: "storage.googleapis.com/amp-public-assets-prod-0/"
