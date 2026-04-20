29cd1f25b126b6f665513dc88748c304a01cb177
Caius Durling <dev@caius.name>
2026-04-20 10:25:09 +0100

    Fix linting issue with swamp formula

--- Formula/swamp.rb
+++ Formula/swamp.rb
@@ -4,6 +4,12 @@ class Swamp < Formula
   version "20260418.005854.0-sha.c17d3e4c"
   license "AGPL-3.0-only"
 
+  livecheck do
+    url :stable
+    strategy :github_releases
+    regex(/^v?(\d+\.\d+\.\d+-sha\.\h+)$/i)
+  end
+
   on_macos do
     on_arm do
       url "https://github.com/systeminit/swamp/releases/download/v#{version}/swamp-darwin-aarch64"
@@ -14,12 +20,6 @@ class Swamp < Formula
       url "https://github.com/systeminit/swamp/releases/download/v#{version}/swamp-darwin-x86_64"
       sha256 "10bf914af65603d6f7437a8fa29cb5d4fde6b001d04a02c09e925d5b9df321f1"
     end
-  end
-
-  livecheck do
-    url :stable
-    strategy :github_releases
-    regex(/^v?(\d+\.\d+\.\d+-sha\.\h+)$/i)
   end
 
   def install
