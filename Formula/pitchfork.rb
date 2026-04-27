f15dcca15a934c287e362a65cb337a0f33cb2179
Anka <runner@iad20-fj918-9a3ea420-80b4-4178-8e95-c3f72b49d873-B6EE7C9FF0B2.local>
2026-04-27 09:04:22 +0000

    pitchfork 2.7.0

--- Formula/pitchfork.rb
+++ Formula/pitchfork.rb
@@ -1,8 +1,8 @@
 class Pitchfork < Formula
   desc "CLI for managing daemons with a focus on developer experience"
   homepage "https://pitchfork.jdx.dev"
-  url "https://github.com/jdx/pitchfork/archive/refs/tags/v2.6.0.tar.gz"
-  sha256 "2037ecb62b130163f125bd88d938b5158d0274f1773c9c6383417699fb40633d"
+  url "https://github.com/jdx/pitchfork/archive/refs/tags/v2.7.0.tar.gz"
+  sha256 "ee200879afe7244773f591f39c50a626b052ac07cca3a47c9dc3c584a9656a40"
   license "MIT"
 
   livecheck do
