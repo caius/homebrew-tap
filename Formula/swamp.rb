f9a688130bdb42272065aaad41ce51299013b249
Anka <runner@sat12-bq161-2435c16a-1fc2-4dea-bd97-836bbcf0fe1d-2E3836177779.local>
2026-04-29 08:59:25 +0000

    swamp 20260428.142235.0-sha.a4bae133

--- Formula/swamp.rb
+++ Formula/swamp.rb
@@ -1,7 +1,7 @@
 class Swamp < Formula
   desc "AI native automation CLI, built for agents"
   homepage "https://github.com/systeminit/swamp"
-  version "20260424.234336.0-sha.0022d1ca"
+  version "20260428.142235.0-sha.a4bae133"
   license "AGPL-3.0-only"
 
   livecheck do
@@ -12,8 +12,8 @@ class Swamp < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/systeminit/swamp/releases/download/v20260424.234336.0-sha.0022d1ca/swamp-darwin-aarch64"
-      sha256 "495cffbb874f234ce5243642692638318f55e8a13bf7059c9c773182a1b0b6ed"
+      url "https://github.com/systeminit/swamp/releases/download/v20260428.142235.0-sha.a4bae133/swamp-darwin-aarch64"
+      sha256 "f9226578be0c19cd1e0863de9d629e2a6e1716f954a9a071f1e91b7b197b402c"
     end
 
     on_intel do
