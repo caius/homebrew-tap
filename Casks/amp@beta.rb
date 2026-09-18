63eec6084283be7e02af4651553005c726eb266c
Anka <runner@iad20-eo1211-fe1451f3-4cfb-404f-b8f3-5538710f09b8-EEE8A78899B7.local>
2026-09-18 08:29:11 +0000

    amp@beta 1.0,417

--- Casks/amp@beta.rb
+++ Casks/amp@beta.rb
@@ -1,6 +1,6 @@
 cask "amp@beta" do
-  version "1.0,406"
-  sha256 "e8be036cb9f2071667dca4a3b61c233e45350f8f9e64726a10561cd4882230a2"
+  version "1.0,417"
+  sha256 "5ce6f4869056823835c7b08513d6db12ed5fef6ec06127c4343843abc0da5985"
 
   url "https://static.ampcode.com/mac/Amp-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Amp"
